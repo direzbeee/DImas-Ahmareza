280200670a22c024016c056b52a2eccc678726ae
Dimas Ahmareza <dimasahmareza122@gmail.com>
2022-11-22 06:41:51 +0700

    Ujian Kompre

---
+++ index.php
@@ -0,0 +1,232 @@
+<?php
+$host       = "localhost";
+$user       = "root";
+$pass       = "";
+$db         = "akademik";
+
+$koneksi    = mysqli_connect($host, $user, $pass, $db);
+if (!$koneksi) { //cek koneksi
+    die("Tidak bisa terkoneksi ke database");
+}
+$nim        = "";
+$nama       = "";
+$alamat     = "";
+$fakultas   = "";
+$Umur       = "";
+$sukses     = "";
+$error      = "";
+
+if (isset($_GET['op'])) {
+    $op = $_GET['op'];
+} else {
+    $op = "";
+}
+if($op == 'delete'){
+    $id         = $_GET['id'];
+    $sql1       = "delete from mahasiswa where id = '$id'";
+    $q1         = mysqli_query($koneksi,$sql1);
+    if($q1){
+        $sukses = "Berhasil hapus data";
+    }else{
+        $error  = "Gagal melakukan delete data";
+    }
+}
+if ($op == 'edit') {
+    $id         = $_GET['id'];
+    $sql1       = "select * from mahasiswa where id = '$id'";
+    $q1         = mysqli_query($koneksi, $sql1);
+    $r1         = mysqli_fetch_array($q1);
+    $nim        = $r1['nim'];
+    $nama       = $r1['nama'];
+    $alamat     = $r1['alamat'];
+    $fakultas   = $r1['fakultas'];
+    $Umur       = $r1['Umur'];
+
+    if ($nim == '') {
+        $error = "Data tidak ditemukan";
+    }
+}
+if (isset($_POST['simpan'])) { //untuk create
+    $nim        = $_POST['nim'];
+    $nama       = $_POST['nama'];
+    $alamat     = $_POST['alamat'];
+    $fakultas   = $_POST['fakultas'];
+    $Umur       = $_POST['Umur'];
+
+    if ($nim && $nama && $alamat && $fakultas && $Umur) {
+        if ($op == 'edit') { //untuk update
+            $sql1       = "update mahasiswa set nim = '$nim',nama='$nama',alamat = '$alamat',fakultas='$fakultas',Umur='$Umur' where id = '$id'";
+            $q1         = mysqli_query($koneksi, $sql1);
+            if ($q1) {
+                $sukses = "Data berhasil diupdate";
+            } else {
+                $error  = "Data gagal diupdate";
+            }
+        } 
+        else { //untuk insert
+            $sql1 = "insert into mahasiswa (nim,nama,alamat,fakultas,Umur) values('$nim','$nama','$alamat','$fakultas', '$Umur')";
+            // $sql1   = "insert into" mahasiswa(nim,nama,alamat,fakultas,Umur)"values ('$nim','$nama','$alamat','$fakultas'$Umur')";
+            $q1     = mysqli_query($koneksi, $sql1);
+            if ($q1) {
+                $sukses     = "Berhasil memasukkan data baru";
+            } else {
+                $error      = "Gagal memasukkan data";
+            }
+        }
+    } else {
+        $error = "Silakan masukkan semua data";
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Mahasiswa</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <style>
+        .mx-auto {
+            width: 800px
+        }
+
+        .card {
+            margin-top: 10px;
+        }
+    </style>
+</head>
+<body>
+    <div class="mx-auto">
+        <!-- untuk memasukkan data -->
+        <div class="card">
+            <img src="YAMAHA.JFIF" class="img-thumbnail" alt="..."width="50" height="50">
+            <div class="card-header">
+            <h2> <center><U>
+                <p class="font-italic">TAMBAHKAN DATA</p>
+              
+            </h2> </center>
+            <div class="card-body">
+                <?php
+                if ($error) {
+                ?>
+                    <div class="alert alert-danger" role="alert">
+                        <?php echo $error ?>
+                    </div>
+                <?php
+                    header("refresh:5;url=index.php");//5 : detik
+                }
+                ?>
+                <?php
+                if ($sukses) {
+                ?>
+                    <div class="alert alert-success" role="alert">
+                        <?php echo $sukses ?>
+                    </div>
+
+                <?php
+                    header("refresh:5;url=index.php");
+                }
+                ?>
+                <form action="" method="POST">
+                    <div class="mb-3 row">
+                        <label for="nim" class="col-sm-2 col-form-label">NIM</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="nim" name="nim" value="<?php echo $nim ?>">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="nama" class="col-sm-2 col-form-label">Nama</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="nama" name="nama" value="<?php echo $nama ?>">
+                        </div>
+                    </div>
+                    <div class="mb-3 row">
+                        <label for="alamat" class="col-sm-2 col-form-label">Alamat</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="alamat" name="alamat" value="<?php echo $alamat ?>">
+                        </div>
+                    </div>
+                     </div> 
+                    <div class="mb-3 row">
+                        <label for="fakultas" class="col-sm-2 col-form-label">Fakultas</label>
+                        <div class="col-sm-10">
+                            <select class="form-control" name="fakultas" id="fakultas">
+                                <option value="">- Pilih Fakultas -</option>
+                                <option value="informatika" <?php if ($fakultas == "informatika") echo "selected" ?>>informatika</option>
+                                <option value="hukum" <?php if ($fakultas == "hukum") echo "selected" ?>>hukum</option>
+                                <option value="ekonomi" <?php if ($fakultas == "ekonomi") echo "selected" ?>>ekonomi</option>
+                            </select>
+                        </div>
+                        <div class="mb-3 row">
+                        <label for="Umur" class="col-sm-2 col-form-label mt-3">Umur</label>
+                        <div class="col-sm-10 mt-3">
+                        <input type="text" class="form-control" id="Umur" name="Umur" value="<?php echo $Umur ?>">
+                        </div>
+                    <div class="col-12">
+                        <input type="submit" name="simpan" value="Simpan Data" class="btn btn-primary" />
+                    </div>
+                </form>
+            </div>
+        </div>
+        <!-- untuk mengeluarkan data -->
+         <label for="cari"> Cari Nama</label>
+                <form method="get" action="">
+                    <input type="text" name="cari">
+                </form>
+        <div class="card">
+            <div class="card-header text-white bg-secondary">
+                Data Mahasiswa
+            </div>
+            <div class="card-body">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">NIM</th>
+                            <th scope="col">Nama</th>
+                            <th scope="col">Alamat</th>
+                            <th scope="col">Fakultas</th>
+                            <th scope="col">Umur</th>
+                            <th scope="col">Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $sql2   = "select * from mahasiswa order by id desc";
+                        $q2     = mysqli_query($koneksi, $sql2);
+                        $urut   = 1;
+                        while ($r2 = mysqli_fetch_array($q2)) {
+                            $id         = $r2['id'];
+                            $nim        = $r2['nim'];
+                            $nama       = $r2['nama'];
+                            $alamat     = $r2['alamat'];
+                            $fakultas   = $r2['fakultas'];
+                            $Umur   = $r2['Umur'];
+
+                        ?>
+                            <tr>
+                                <th scope="row"><?php echo $urut++ ?></th>
+                                <td scope="row"><?php echo $nim ?></td>
+                                <td scope="row"><?php echo $nama ?></td>
+                                <td scope="row"><?php echo $alamat ?></td>
+                                <td scope="row"><?php echo $fakultas ?></td>
+                                <td scope="row"><?php echo $Umur?></td>
+                                <td scope="row">
+                                    <a href="index.php?op=edit&id=<?php echo $id ?>"><button type="button" class="btn btn-warning">Edit</button></a>
+                                    <a href="index.php?op=delete&id=<?php echo $id?>" onclick="return confirm('Yakin mau delete data?')"><button type="button" class="btn btn-danger">Delete</button></a>            
+                                </td>
+                            </tr>
+                        <?php
+                        }
+                        ?>
+                    </tbody>
+                    
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ mahasiswa.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 22, 2022 at 12:32 AM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 7.4.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `akademik`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `id` int(11) NOT NULL,
+  `nim` varchar(25) NOT NULL,
+  `nama` varchar(25) NOT NULL,
+  `alamat` varchar(25) NOT NULL,
+  `fakultas` varchar(25) NOT NULL,
+  `Umur` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`id`, `nim`, `nama`, `alamat`, `fakultas`, `Umur`) VALUES
+(4, '20200003', 'Tri Untoro', 'Semarang Tengah', 'soshum', '22'),
+(5, '20200004', 'Ahmed Salah', 'Yogyakarta', 'hukum', '24'),
+(6, '20200005', 'Neymar junior', 'Jakarta', 'ekonomi', '21'),
+(7, '20200006', 'David Beckham', 'inggris', 'hukum', '23'),
+(8, '12345678', 'Cirstiano Ronaldo', 'Portugal', 'informatika', '38'),
+(10, '18203847', 'Messi', 'Argentina', 'informatika', '37');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nim` (`nim`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
